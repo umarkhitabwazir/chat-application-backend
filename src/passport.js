0b4c70ef9039d83ce6364eca9cb2d96dea943d09
umar khitab <umair123123ss@gmail.com>
2025-08-05 20:20:01 +0500

    Fix: Correctly access GOOGLE_CLIENT_SECRET from process.env in Google OAuth strategy

--- src/passport.js
+++ src/passport.js
@@ -9,7 +9,7 @@ passport.use(
     new GoogleStrategy(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.GOOGLE_CLIENT_SECRET,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
             callbackURL: `/api/auth/google/callback`,
             scope: ['profile', 'email'],
         },
